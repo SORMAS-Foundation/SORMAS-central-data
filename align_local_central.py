f94c76a210f17d2ab2f6a801822d87be039a3638
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-12-15 17:47:53 +0100

    script to align local with central

--- align_local_central.py
+++ align_local_central.py
@@ -1,12 +1,26 @@
 import psycopg
 import json
+
+from psycopg.errors import UniqueViolation
 from psycopg.rows import dict_row
 
 import logging
 
-logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+error_list = []
+
+FORMAT = '%(message)s'
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG, format=FORMAT)
 
 CONNECTION = "host= localhost dbname=sormas_db user=postgres password=postgres"
+
+
+def archive_everything(table):
+    logging.info(f"Archive {table}")
+    with psycopg.connect(CONNECTION) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute("UPDATE featureconfiguration SET enabled=FALSE WHERE featuretype='EDIT_INFRASTRUCTURE_DATA';")
+            cur.execute(f"UPDATE {table} SET archived=TRUE WHERE TRUE;")
 
 
 def iterate_central():
@@ -23,85 +37,137 @@ def iterate_central():
         logging.info(f"Process table {table}")
         with open(path) as f:
             entities: list = json.load(f)
+        # STEP 1: invalidate ALL present data and prevent user from interfering
+        archive_everything(table)
+
         for entity in sorted(entities, key=lambda kv: kv['key']):
             central_value = entity['value']
-            # 1. Check for UUID match
-            if check_local_uuid(table, central_value):
-                logging.info(f"Exact local match! Skipping {table}: {central_value['uuid']}")
+
+            name = central_value['defaultName'] if has_default_name(table) else central_value['name']
+            id_ = central_value['externalId'] if central_value.get('externalId') else central_value['externalID']
+            uuid_ = central_value['uuid']
+            logging.info(f"Processing {name}, {id_}, {uuid_}")
+            # STEP 2 check if central item is locally present by uuid
+            if update_by_local_uuid(table, central_value):
                 continue
-            if check_local_name_and_id(table, central_value):
+            if update_by_local_name_and_id(table, central_value):
                 continue
-            # insert value
 
 
-def check_local_name_and_id(table, central_value):
+def update_by_local_uuid(table, central_value):
     with psycopg.connect(CONNECTION) as conn:
         with conn.cursor(row_factory=dict_row) as cur:
-            where_name = central_value['defaultName'] if has_default_name(table) else central_value['name']
-            external_id = central_value['externalId'] if central_value.get('externalId') else central_value[
-                'externalID']
-            where_ext_id = f"externalid={external_id}"
+            local = cur.execute(f"SELECT * FROM {table} WHERE uuid=%s", [central_value['uuid']]).fetchone()
+            if local:
+                perform_update(central_value, local, table, cur)
+                logging.info(f"\t\tExact local uuid match de-archived and name/external id aligned!")
+                return True
+            else:
+                logging.info(f"\t\tNo exact local uuid match! Continue with name and external id lookup!")
+                return False
+
+
+def update_by_local_name_and_id(table, central_value):
+    with psycopg.connect(CONNECTION) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            where_ext_id, where_name = get_where_clause(central_value, table)
+
             if has_default_name(table):
                 local = cur.execute(f"SELECT * FROM {table} WHERE defaultname=%s OR externalid=%s;",
                                     (where_name, where_ext_id)).fetchone()
             else:
                 local = cur.execute(f"SELECT * FROM {table} WHERE name=%s OR externalid=%s;",
                                     (where_name, where_ext_id)).fetchone()
+
             if local:
-                # we found a local match which is also present in central, but with a different UUID
-                uuid_ = central_value['uuid']
-                # update local UUID
-                if has_default_name(table):
-                    cur.execute(f"UPDATE {table} SET uuid=%s WHERE defaultname=%s OR externalid=%s;",
-                                (uuid_, where_name, where_ext_id))
-                else:
-                    logging.info(f"Updating {table} from {local['uuid']} to {uuid_}")
-                    cur.execute(f"UPDATE {table} SET uuid=%s WHERE name=%s OR externalid=%s;",
-                                (uuid_, where_name, where_ext_id))
-
+                perform_update(central_value, local, table, cur)
             else:
-                logging.error(
-                    f"Could not find local item in {table} with external ID {external_id} and {where_name}")
-
-
-def check_local_uuid(table, central_value):
-    with psycopg.connect(CONNECTION) as conn:
-        with conn.cursor(row_factory=dict_row) as cur:
-            local = cur.execute(f"SELECT * FROM {table} WHERE uuid=%s", [central_value['uuid']]).fetchone()
-            if local:
-                central_ext_id = central_value['externalId'] if central_value.get('externalId') else central_value[
-                    'externalID']
-                local_ext_id = local['externalid']
-                ext_id = central_ext_id == local_ext_id
-                central_name = central_value[
-                    'defaultName' if has_default_name(table) else 'name']
-                local_name = local[
-                    'defaultname' if has_default_name(table) else 'name']
-                name = central_name == local_name
-                sanity = ext_id and name
-                if not sanity:
-                    logging.error(f"Sanity check failed for {table}:"
-                                  f"\tCentral: {central_name}, {central_ext_id}"
-                                  f"\tLocal: {local_name}, {local_ext_id}")
-                return sanity
-            else:
+                report_error(
+                    f"\t\tCould not find local item present in central({central_value['uuid']}, {where_name}, {where_ext_id})")
                 return False
+
+
+def perform_update(central_value, local, table, cur):
+    if not sanity_check(central_value, local, table):
+        return
+
+    local_uuid = local['uuid']
+    local_name = get_local_name(local)
+    local_ext_id = local['externalid']
+    central_uuid = central_value['uuid']
+    where_ext_id, where_name = get_where_clause(central_value, table)
+
+    try:
+        if has_default_name(table):
+            cur.execute(
+                f"UPDATE {table} SET uuid=%s,archived=FALSE,defaultname=%s,externalid=%s WHERE uuid=%s OR (defaultname=%s OR externalid=%s);",
+                (central_uuid, where_name, where_ext_id, central_uuid, where_name, where_ext_id))
+        else:
+            cur.execute(
+                f"UPDATE {table} SET uuid=%s,archived=FALSE,name=%s,externalid=%s WHERE uuid=%s OR (name=%s OR externalid=%s);",
+                (central_uuid, where_name, where_ext_id, central_uuid, where_name, where_ext_id))
+
+        uuid_changed = f"UUID: {local_uuid} -> {central_uuid}" if local_uuid != central_uuid else ""
+        name_changed = f"Name: {local_name} -> {where_name}" if local_name != where_name else ""
+        ext_id_changed = f"Ext. ID: {local_ext_id} -> {where_ext_id}" if local_ext_id != where_ext_id else ""
+
+        logging.info(
+            f"\t\tUpdated local item ({','.join([uuid_changed, name_changed, ext_id_changed])})")
+        return True
+    except UniqueViolation as e:
+        report_error(
+            f"\t\tCould not update UUID of {local_uuid}: {where_name}, {where_ext_id}"
+            f"Either the name or external ID are present multiple times in the local DB.")
+        return False
+
+
+def get_local_name(local):
+    return local['name'] if local.get('name') else local['defaultname']
+    pass
+
+
+def get_where_clause(central_value, table):
+    where_name = central_value['defaultName'] if has_default_name(table) else central_value['name']
+    where_ext_id = central_value['externalId'] if central_value.get('externalId') else central_value['externalID']
+    return where_ext_id, where_name
+
+
+def sanity_check(central_value, local, table):
+    central_ext_id = central_value['externalId'] if central_value.get('externalId') else central_value[
+        'externalID']
+    local_ext_id = local['externalid']
+    ext_id = central_ext_id == local_ext_id
+    central_name = central_value[
+        'defaultName' if has_default_name(table) else 'name']
+    local_name = local[
+        'defaultname' if has_default_name(table) else 'name']
+    name = central_name == local_name
+
+    # we allow either one to match
+    # IMPORTANT: In the update we update both the name and external ID from central
+    sanity = ext_id or name
+
+    if not sanity:
+        logging.info(
+            f"\t\tSanity check failed for {table}: Central: {central_name}, {central_ext_id} and Local: {local_name}, {local_ext_id}")
+
+    return sanity
 
 
 def has_default_name(table):
     return table in ['continent', 'subcontinent', 'country']
 
 
-def iterate_local():
-    pass
-
-
-# Connect to an existing database
+def report_error(param):
+    logging.error(param)
+    error_list.append(f'{param.strip()}\n')
 
 
 def main():
     iterate_central()
     logging.info("All done")
+    with open('errors.log', 'w+') as f:
+        f.writelines(error_list)
 
 
 if __name__ == '__main__':
---
+++ import/locations/import.sh
@@ -0,0 +1,7 @@
+export PGPASSWORD=postgres
+cat continent.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
+cat subcontinent.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
+cat country.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
+cat region.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
+cat district.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
+cat community.sql | psql  --host=localhost --port=5432 -U postgres -d sormas_db
